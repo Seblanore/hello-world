46b2cd001f51f63f84b59834c263a3930870f847
Seblanore <seb.lanore@gmail.com>
2023-07-18 21:41:24 +0200

    Adding some circle - in a circle

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,88 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <h2>Vue.js and D3 Line Chart</h2>
+    <svg></svg>
   </div>
 </template>
-
 <script>
+import * as d3 from "d3";
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
+    mounted() {
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+    const width = 1000;
+    const height = 1000;
+
+    var spaceCircles = [
+        {
+            "name": "Categorie 1",
+            "color": "red",
+            "angle": 0
+        },
+        {
+            "name": "Categorie 2",
+            "color": "blue",
+            "angle": 30
+        },
+        {
+            "name": "Categorie 3",
+            "color": "yellow",
+            "angle": 60
+        },
+        {
+            "name": "Categorie 3",
+            "color": "yellow",
+            "angle": 90
+        },
+        {
+            "name": "Categorie 3",
+            "color": "yellow",
+            "angle": 120
+        },
+        {
+            "name": "Categorie 3",
+            "color": "yellow",
+            "angle": 150
+        },
+        {
+            "name": "Categorie 3",
+            "color": "yellow",
+            "angle": 180
+        },
+        {
+            "name": "Categorie 3",
+            "color": "yellow",
+            "angle": 210
+        } 
+      ];
+
+    var svgContainer = d3.select("body").append("svg")
+      .attr("width", width)
+      .attr("height", height);   
+
+    var circles = svgContainer.selectAll("circle")
+      .data(spaceCircles)
+      .enter()
+      .append("circle");     
+          
+    circles
+      .attr("cx", function (d) { return width/2 + Math.sin(d.angle)*100; })
+      .attr("cy", function (d) { return height/2 + Math.cos(d.angle)*100; })
+      .attr("r", 20 )
+      .style("fill", function(d) {
+                return d.color;
+              });
+
+      svgContainer.append("circle")
+      .attr("cx",width/2)
+      .attr("cy",height/2)
+      .attr("r", 100)
+      .style("fill", "red");
+
+      svgContainer.append("circle")
+      .attr("cx",width/2)
+      .attr("cy",height/2)
+      .attr("r", 2)
+      .style("fill", "black");
+  },
+};
+</script>
